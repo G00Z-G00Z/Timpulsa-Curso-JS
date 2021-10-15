fcc003e04269188500400e5ce451c3f85513e83e
Eduardo <edygomezgg4@gmail.com>
2021-10-15 09:15:30 -0500

    Added styles files

--- kaboomjs/videojuegos/ChooseYourOwnAdventure/code/main.ts
+++ kaboomjs/videojuegos/ChooseYourOwnAdventure/code/main.ts
@@ -1,8 +1,9 @@
-import kaboom, { Origin, TextCompConf } from "kaboom";
+import kaboom, { Origin } from "kaboom";
 import { miHistoria } from './miHistoria';
 import { Escena } from './interfaces';
 import { setUpScene } from "./promtScreen";
 import { Vec2 } from 'kaboom';
+import { addBackground, getLayers, textConfigs, scenesId } from './stylesAndConfigs';
 
 
 kaboom();
@@ -10,51 +11,34 @@ kaboom();
 // Declaration of origin function
 declare function origin(pos: Origin): void
 
-loadSprite("bean", "sprites/bean.png")
 
-const addBackground = (layerId: string) => {
+scene(scenesId.escena, (escena: Escena, previousScene: string) => {
+
+
+    getLayers()
+    addBackground()
+
+    const { layOutConfig, layout } = setUpScene(escena, textConfigs.option)
+    addLevel(layout, layOutConfig)
+
+
+})
+scene(scenesId.final, (escena: Escena, previousScene: string) => {
+
+
+    getLayers()
+    addBackground()
+
     add([
-        rect(width(), height()),
-        color(0, 0, 0),
-        layer(layerId)
+        text(escena.mensaje, textConfigs.prompt)
     ])
-}
 
-
-const textConfig: TextCompConf = {
-    size: width() * .04,
-    width: width() * .9,
-
-}
-
-
-
-
-scene("escena", (escena: Escena, previousScene: string) => {
-
-    layers([
-        "background",
-        "ui",
-    ], "ui")
-    addBackground("background")
-
-
-    if (escena.esFinal) {
-        add([
-            text(escena.mensaje, textConfig)
-        ])
-
-        addButton("Regresar al inicio?", vec2(width() / 2, height() / 2), () => {
-            go("escena", miHistoria.getScene("1"), "1")
-        })
-        addButton("Regresar escena anterior", vec2(width() / 2, 3 * height() / 4), () => {
-            go("escena", miHistoria.getScene(previousScene), "1")
-        })
-
-    } else {
-        const { layOutConfig, layout } = setUpScene(escena, textConfig)
-        addLevel(layout, layOutConfig)
-    }
+    addButton("Regresar al inicio?", vec2(width() / 2, height() / 2), () => {
+        go("escena", miHistoria.getScene("1"), "1")
+    })
+    addButton("Regresar escena anterior", vec2(width() / 2, 3 * height() / 4), () => {
+        go("escena", miHistoria.getScene(previousScene), "1")
+    })
 
 })
 
@@ -64,7 +48,7 @@ function addButton(txt: string, p: Vec2, f: () => void) {
 
     const btn = add([
         rect(10, 10),
-        text(txt, textConfig),
+        text(txt, textConfigs.prompt),
         color(255, 255, 0),
         pos(p),
         area({ cursor: "pointer", }),
---
+++ kaboomjs/videojuegos/ChooseYourOwnAdventure/code/stylesAndConfigs.ts
@@ -0,0 +1,49 @@
+import { CompList, TextCompConf } from "kaboom";
+
+
+const layersId: {
+    background: string,
+    ui: string
+} = {
+    background: "background",
+    ui: "ui"
+}
+
+
+export const scenesId: {
+    escena: string,
+    final: string
+} = {
+    escena: "escena",
+    final: "ending"
+}
+
+export const getLayers = () => {
+    layers([
+        layersId.background,
+        layersId.ui,
+    ], layersId.ui)
+}
+
+
+export const addBackground = () => {
+    add([
+        rect(width(), height()),
+        color(0, 0, 0),
+        layer(layersId.background)
+    ])
+}
+
+export const textConfigs: {
+    prompt: TextCompConf,
+    option: TextCompConf
+} = {
+    prompt: {
+        size: width() * 0.4,
+        width: width() * .9
+    },
+    option: {
+        size: width() * 0.4,
+        width: width() * .9
+    }
+}
